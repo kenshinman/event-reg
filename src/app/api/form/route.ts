39c925ef9804f2212f789d2acdbf0de4cd5e312f
Kehinde Orilogbon <archkenshin@gmail.com>
2025-07-19 13:18:39 -0400

    refactor GET function in route.ts to remove unused NextRequest import

--- src/app/api/form/route.ts
+++ src/app/api/form/route.ts
@@ -1,7 +1,7 @@
-import { NextRequest, NextResponse } from 'next/server';
 import { getSheetTitle } from '@/lib/googleSheets';
+import { NextResponse } from 'next/server';
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   try {
     const title = await getSheetTitle();
     return NextResponse.json({ title });
